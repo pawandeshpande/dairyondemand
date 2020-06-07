b51091ac86ccbb1dbba164383ba86da900f27e89
pawan.deshpande@gmail.com <pawan.deshpande@gmail.com>
2020-06-07 17:38:25 +0530

    Fixed a bug related to community store link in the site landing page by adding cookie

--- hhub/dod-ui-cus.lisp
+++ hhub/dod-ui-cus.lisp
@@ -1552,7 +1552,7 @@
 	  (setf (hunchentoot:session-value :login-prdcatg-cache) (select-prdcatg-by-company customer-company))
 	  (unless (equal customer-tenant-id *HHUB-DEMO-TENANT-ID*)
 	    (hunchentoot:set-cookie "community-url" :value (format nil "https://www.highrisehub.com/hhub/dascustloginasguest?tenant-id=~A" (get-login-cust-tenant-id)) :expires (+ (get-universal-time) 10000000) :path "/")
-	    (hunchentoot:set-cookie "community-name" :value customer-company-name :path "/")) 
+	    (hunchentoot:set-cookie "community-name" :value customer-company-name :path "/" :expires (+ (get-universal-time) 10000000))) 
 	  ))
       )
 
